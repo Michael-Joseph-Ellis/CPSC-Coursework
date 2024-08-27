e076fd2d89ad7cd100fcc83190de48b5ccedb7bd
Michael-Joseph-Ellis <mje2@clemson.edu>
2024-08-27 18:29:39 +0000

    Synced files from organization repos

---
+++ CPSC2150/first assignment/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        //change the variables from "" to <your name> - <your GitHub username>.
+        //for instance, I would change memberOne to "Matthew Re - Matthewjre"
+        String memberOne = "Michael Ellis - Michael-Joseph-Ellis";
+        String memberTwo = "";
+        String memberThree = "";
+        String memberFour = "";
+
+
+        String msg = "Hello World from " + memberOne + ", " + memberTwo + ", " + memberThree + ", " + memberFour;
+
+        System.out.println(msg);
+    }
+}
